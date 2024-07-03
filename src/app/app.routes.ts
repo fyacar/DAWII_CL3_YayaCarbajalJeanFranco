88aaceae1ce935d1c6f22cc7c17e5e9f4165c3da
Jean Y <fyacar_14@hotmail.com>
2024-07-03 13:21:46 -0500

    Pregunta2 - Primer Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { Pregunta2Component } from './home/pregunta2/pregunta2.component';
 
 export const routes: Routes = [
-    {path:"home", component: HomeComponent},
+    {path:"home", component: HomeComponent,
+        children:[ {path:"pregunta2", component: Pregunta2Component}]
+    },
+    
+   
     {path:"", redirectTo: "home", pathMatch: "full"},
     {path:"**", component: PageNotFoundComponent}
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../angular-material/material/material.module';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [RouterOutlet,MaterialModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  constructor(private router: Router,
+    private route:ActivatedRoute){   
+  }
+
+
+  irPregunta2():void{
+    this.router.navigate(["pregunta2"], {relativeTo: this.route})
+  }
+
 
 }
